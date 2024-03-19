21fe741963802367e45f13b897a082a5a9871852
Daazzer <w513765485@163.com>
2024-03-19 15:52:00 +0800

    feat: 完善首页

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import store from './store';
 import { Provider } from 'react-redux';
-import './index.scss';
+import store from './store';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import './index.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
--- src/pages/Index/index.js
+++ src/pages/Index/index.js
@@ -1,5 +1,37 @@
+import { createElement, useState } from 'react';
+import classNames from 'classnames';
+import { BsCheckCircleFill, BsCircle } from 'react-icons/bs';
+import { uuid } from '@/utils';
+import './index.scss';
+
+const todos = Array(8).fill().map(() => uuid());
+
 export default function Index() {
+  const [checkedTodos, setCheckedTodos] = useState(todos.slice(0, 3));
+
   return (
-    <div>Index</div>
+    <div className="index">
+      <h1 className="index__title">Todoist Demo</h1>
+      <ul className="todo">
+        {todos.map(item => {
+          const isChecked = checkedTodos.includes(item);
+          return (
+            <li key={item} className="todo__item">
+              {createElement(isChecked ? BsCheckCircleFill : BsCircle, {
+                className: classNames('todo__item__icon', { checked: isChecked }),
+                onClick() {
+                  let state = checkedTodos.concat(item);
+                  if (isChecked) {
+                    state = checkedTodos.filter(checkedTodo => checkedTodo !== item);
+                  }
+                  setCheckedTodos(state);
+                }
+              })}
+              <div className={classNames('todo__item__content', { checked: isChecked })} />
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 }
